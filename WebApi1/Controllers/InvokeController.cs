09a44de191c04f7dbd5060515403440ce855acbd
Carlos Pons <karlospn@gmail.com>
2020-10-13 13:05:48 +0200

    Add initial commit

---
+++ WebApi1/Controllers/InvokeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
+
+namespace WebApi1.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/invoke")]
+    public class InvokeController : ControllerBase
+    {
+
+        private readonly ILogger<InvokeController> _logger;
+        private IDownstreamWebApi _downstreamWebApi;
+
+        public InvokeController(ILogger<InvokeController> logger, 
+            IDownstreamWebApi downstreamWebApi)
+        {
+            _logger = logger;
+            _downstreamWebApi = downstreamWebApi;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            _logger.LogInformation("Trying to call a downstream Api");
+            var value = await _downstreamWebApi.CallWebApiForUserAsync(
+                "WebApi2",
+                options =>
+                {
+                    options.HttpMethod = HttpMethod.Get;
+                    options.RelativePath = "api/downstream";
+                });
+
+            return Ok(value);
+        }
+    }
+}
---
+++ WebApi2/Controllers/DownstreamController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi2.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("/api/downstream")]
+    public class DownstreamController : ControllerBase
+    {
+
+        private readonly ILogger<DownstreamController> _logger;
+
+        public DownstreamController(ILogger<DownstreamController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>
+            {
+                "hello",
+                "world"
+            };
+        }
+    }
+}
